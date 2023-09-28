85e59ca456a288f21299e1eb3db44a5dd4a7ed23
Beme <60401038+Kis-Benjamin@users.noreply.github.com>
2023-09-28 15:16:46 +0200

    Optimized main container

--- LudoCompose/app/src/main/java/hu/bme/aut/android/ludocompose/MainActivity.kt
+++ LudoCompose/app/src/main/java/hu/bme/aut/android/ludocompose/MainActivity.kt
@@ -3,8 +3,8 @@ package hu.bme.aut.android.ludocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -14,7 +14,7 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -26,6 +26,7 @@ import hu.bme.aut.android.ludocompose.ui.common.LudoAppBar
 import hu.bme.aut.android.ludocompose.ui.theme.LudoComposeTheme
 
 @AndroidEntryPoint
+@ExperimentalAnimationApi
 @ExperimentalMaterial3Api
 @ExperimentalFoundationApi
 @ExperimentalComposeUiApi
@@ -40,27 +41,24 @@ class MainActivity : ComponentActivity() {
                 ) {
                     val snackbarHostState = remember { SnackbarHostState() }
 
-                    var titleId by remember { mutableStateOf(R.string.app_name) }
+                    var titleId by remember { mutableIntStateOf(R.string.app_name) }
 
                     Scaffold(
                         snackbarHost = { SnackbarHost(snackbarHostState) },
                         topBar = {
                             LudoAppBar(
-                                title = stringResource(id = titleId),
+                                title = stringResource(titleId),
                             )
                         },
                         floatingActionButton = {}
                     ) { paddingValues ->
-                        Column(
+                        NavGraph(
+                            snackbarHostState = snackbarHostState,
+                            onTitleChange = { titleId = it },
                             modifier = Modifier
                                 .fillMaxSize()
                                 .padding(paddingValues)
-                        ) {
-                            NavGraph(
-                                snackbarHostState = snackbarHostState,
-                                onTitleChange = { titleId = it },
-                            )
-                        }
+                        )
                     }
                 }
             }
