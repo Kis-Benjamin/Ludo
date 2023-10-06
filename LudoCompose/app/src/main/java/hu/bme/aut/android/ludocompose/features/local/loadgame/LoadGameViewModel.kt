b4e381add2b989b14977d4474b56aac0ef5181e4
Kis Benjamin <60401038+Kis-Benjamin@users.noreply.github.com>
2023-10-06 11:43:14 +0200

    Updated Load Game Screen

--- LudoCompose/app/src/main/java/hu/bme/aut/android/ludocompose/features/local/loadgame/LoadGameViewModel.kt
+++ LudoCompose/app/src/main/java/hu/bme/aut/android/ludocompose/features/local/loadgame/LoadGameViewModel.kt
@@ -4,23 +4,18 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import hu.bme.aut.android.ludocompose.R
-import hu.bme.aut.android.ludocompose.domain.model.Board
 import hu.bme.aut.android.ludocompose.domain.usecases.DeleteGameUseCase
 import hu.bme.aut.android.ludocompose.domain.usecases.LoadGameUseCase
 import hu.bme.aut.android.ludocompose.domain.usecases.LoadGamesUseCase
 import hu.bme.aut.android.ludocompose.ui.model.GameListItemUi
 import hu.bme.aut.android.ludocompose.ui.model.UiText
 import hu.bme.aut.android.ludocompose.ui.model.toUiModel
-import hu.bme.aut.android.ludocompose.ui.model.toUiText
+import hu.bme.aut.android.ludocompose.ui.util.LoadingViewModel
 import hu.bme.aut.android.ludocompose.ui.util.UiEvent
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.Channel
+import hu.bme.aut.android.ludocompose.ui.util.UiEventViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -33,33 +28,31 @@ class LoadGameViewModel @Inject constructor(
     private val _state = MutableStateFlow(LoadGameState())
     val state = _state.asStateFlow()
 
-    private val _uiEvent = Channel<UiEvent>()
-    val uiEvent = _uiEvent.receiveAsFlow()
+    private val loadingViewModel = LoadingViewModel(
+        coroutineScope = viewModelScope,
+        loadData = ::loadData,
+    )
 
-    fun loadAll() {
-        viewModelScope.launch {
-            _state.update { state ->
-                state.copy(isLoading = true)
-            }
-            try {
-                CoroutineScope(coroutineContext).launch(Dispatchers.IO) {
-                    val games = loadGamesUseCase().getOrThrow().map { it.toUiModel() }
-                    _state.update { state ->
-                        state.copy(
-                            isLoading = false,
-                            games = games,
-                        )
-                    }
-                }
-            } catch (e: Exception) {
-                _state.update { state ->
-                    state.copy(
-                        isLoading = false,
-                        error = e,
-                    )
-                }
-            }
+    private val uiEventViewModel = UiEventViewModel(
+        coroutineScope = viewModelScope,
+        events = mapOf(
+            "loadSelected" to ::loadSelectedEvent,
+            "delete" to ::deleteEvent,
+        ),
+    )
+
+    val loadingState get() = loadingViewModel.state
+
+    val uiEvent get() = uiEventViewModel.uiEvent
+
+    private suspend fun loadData() = try {
+        val games = loadGamesUseCase().getOrThrow().map { it.toUiModel() }
+        _state.update { state ->
+            state.copy(games = games)
         }
+        Result.success(Unit)
+    } catch (e: Exception) {
+        Result.failure(e)
     }
 
     fun select(index: Int) {
@@ -67,51 +60,37 @@ class LoadGameViewModel @Inject constructor(
             state.copy(selectedIndex = index)
         }
     }
+    private suspend fun loadSelectedEvent(data: Any?) {
+        val selectedId = _state.value.selectedId
+        if (selectedId == null) {
+            val uiText = UiText.StringResource(R.string.load_game_empty_selection)
+            val uiEvent = UiEvent.Failure(uiText)
+            uiEventViewModel.send(uiEvent)
+            return
+        }
+        loadGameUseCase(selectedId)
+        uiEventViewModel.send(UiEvent.Success)
+    }
 
     fun loadSelected() {
-        viewModelScope.launch {
-            try {
-                val selectedId = _state.value.selectedId
-                if (selectedId == null) {
-                    _uiEvent.send(UiEvent.Failure(UiText.StringResource(R.string.load_game_empty_selection)))
-                    return@launch
-                }
-                CoroutineScope(coroutineContext).launch(Dispatchers.IO) {
-                    loadGameUseCase(selectedId)
-                }
-                _uiEvent.send(UiEvent.Success)
-            } catch (e: Exception) {
-                _uiEvent.send(UiEvent.Failure(e.toUiText()))
-            }
-        }
+        uiEventViewModel.fire("loadSelected")
+    }
+
+    private suspend fun deleteEvent(data: Any?) {
+        val id = data as Long
+        deleteGameUseCase(id)
+        loadingViewModel.load(true)
     }
 
     fun delete(id: Long) {
-        viewModelScope.launch {
-            try {
-                CoroutineScope(coroutineContext).launch(Dispatchers.IO) {
-                    deleteGameUseCase(id)
-                    _state.update { state ->
-                        state.copy(
-                            games = state.games.filter { it.id != id },
-                            selectedIndex = null,
-                        )
-                    }
-                }
-            } catch (e: Exception) {
-                _uiEvent.send(UiEvent.Failure(e.toUiText()))
-            }
-        }
+        uiEventViewModel.fire("delete", id)
     }
 }
 
 data class LoadGameState(
-    val isLoading: Boolean = false,
-    val error: Throwable? = null,
     val games: List<GameListItemUi> = emptyList(),
     val selectedIndex: Int? = null,
 ) {
-    val isError: Boolean get() = error != null
     val selectedId: Long?
         get() = selectedIndex?.let { games.getOrNull(it)?.id }
 }
