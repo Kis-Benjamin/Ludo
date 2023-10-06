19ce6907c6c605d4479620ad5e693ef5e1e40f5d
Kis Benjamin <60401038+Kis-Benjamin@users.noreply.github.com>
2023-10-06 19:38:49 +0200

    Added coroutine exception handling

--- LudoCompose/app/src/main/java/hu/bme/aut/android/ludocompose/ui/util/LoadingViewModel.kt
+++ LudoCompose/app/src/main/java/hu/bme/aut/android/ludocompose/ui/util/LoadingViewModel.kt
@@ -1,40 +1,43 @@
 package hu.bme.aut.android.ludocompose.ui.util
 
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.yield
 
 class LoadingViewModel(
     private val coroutineScope: CoroutineScope,
-    private val loadData: suspend () -> Result<Unit>,
+    private val loadData: suspend () -> Unit,
 ) {
 
     private val _state = MutableStateFlow(LoadingState())
     val state = _state.asStateFlow()
+
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        coroutineScope.launch {
+            _state.update { state ->
+                state.copy(isLoading = false, error = throwable)
+            }
+        }
+    }
 
     init {
         load()
     }
 
     fun load(requestReset: Boolean = false) {
-        coroutineScope.launch {
+        coroutineScope.launch(exceptionHandler) {
             if (requestReset) {
                 _state.update { state ->
                     state.copy(isLoading = true)
                 }
             }
-            val result = loadData()
+            loadData() // Possible to throw exception
             _state.update { state ->
-                result.fold(
-                    onSuccess = {
-                        state.copy(isLoading = false)
-                    },
-                    onFailure = {
-                        state.copy(isLoading = false, error = it)
-                    }
-                )
+                state.copy(isLoading = false)
             }
         }
     }
--- LudoCompose/app/src/main/java/hu/bme/aut/android/ludocompose/ui/util/UiEventViewModel.kt
+++ LudoCompose/app/src/main/java/hu/bme/aut/android/ludocompose/ui/util/UiEventViewModel.kt
@@ -1,5 +1,7 @@
 package hu.bme.aut.android.ludocompose.ui.util
 
+import hu.bme.aut.android.ludocompose.ui.model.toUiText
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
@@ -12,9 +14,17 @@ class UiEventViewModel(
     private val _uiEvent = Channel<UiEvent>()
     val uiEvent = _uiEvent.receiveAsFlow()
 
-    fun fire(event: String, data: Any? = null) {
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
         coroutineScope.launch {
-            val result = events[event]!!.invoke(data)
+            _uiEvent.send(UiEvent.Failure(throwable.toUiText()))
+        }
+    }
+
+    fun fire(event: String, data: Any? = null) {
+        coroutineScope.launch(exceptionHandler) {
+            val result = checkNotNull(events[event]?.invoke(data)) {
+                "Event $event not found"
+            }
             _uiEvent.send(result)
         }
     }
