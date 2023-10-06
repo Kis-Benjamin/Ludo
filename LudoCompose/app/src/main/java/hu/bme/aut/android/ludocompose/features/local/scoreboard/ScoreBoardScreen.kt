d88fe96c14bfa7d110a9550f7f93b79736284e7f
Kis Benjamin <60401038+Kis-Benjamin@users.noreply.github.com>
2023-10-06 09:47:33 +0200

    Updated Score Board Screen

--- LudoCompose/app/src/main/java/hu/bme/aut/android/ludocompose/features/local/scoreboard/ScoreBoardScreen.kt
+++ LudoCompose/app/src/main/java/hu/bme/aut/android/ludocompose/features/local/scoreboard/ScoreBoardScreen.kt
@@ -2,7 +2,6 @@ package hu.bme.aut.android.ludocompose.features.local.scoreboard
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,8 +11,6 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
@@ -21,117 +18,68 @@ import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import hu.bme.aut.android.ludocompose.R
-import hu.bme.aut.android.ludocompose.ui.model.toUiText
-import hu.bme.aut.android.ludocompose.ui.util.UiEvent
-import kotlinx.coroutines.launch
+import hu.bme.aut.android.ludocompose.ui.common.LoadingScreen
+import hu.bme.aut.android.ludocompose.ui.common.UiEventHandler
 
 @Composable
 fun ScoreBoardScreen(
     snackbarHostState: SnackbarHostState,
     scoreBoardViewModel: ScoreBoardViewModel = hiltViewModel()
 ) {
+    val loadingState by scoreBoardViewModel.loadingState.collectAsStateWithLifecycle()
     val state by scoreBoardViewModel.state.collectAsStateWithLifecycle()
 
-    val scope = rememberCoroutineScope()
+    UiEventHandler(scoreBoardViewModel.uiEvent, snackbarHostState)
 
-    val context = LocalContext.current
+    LoadingScreen(loadingState) {
+        if (state.scores.isEmpty()) {
+            Text(text = stringResource(id = R.string.score_board_empty))
+        } else {
+            val shape = RoundedCornerShape(15.dp)
 
-    scoreBoardViewModel.loadAll()
-
-    LaunchedEffect(key1 = true) {
-        scoreBoardViewModel.uiEvent.collect { uiEvent ->
-            when (uiEvent) {
-                is UiEvent.Success -> {}
-
-                is UiEvent.Failure -> {
-                    scope.launch {
-                        snackbarHostState.showSnackbar(uiEvent.message.asString(context))
-                    }
-                }
-            }
-        }
-    }
-
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(15.dp)
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .clip(RoundedCornerShape(10.dp))
-                .background(
-                    color = if (!state.isLoading && !state.isError) {
-                        colorScheme.secondaryContainer
-                    } else {
-                        colorScheme.background
-                    }
-                ),
-            contentAlignment = Alignment.Center
-        ) {
-            if (state.isLoading) {
-                CircularProgressIndicator(
-                    color = colorScheme.secondaryContainer
-                )
-            } else if (state.isError) {
-                Text(
-                    text = state.error?.toUiText()?.asString(context)
-                        ?: stringResource(id = R.string.unknown_error_message)
-                )
-            } else {
-                if (state.scores.isEmpty()) {
-                    Text(text = stringResource(id = R.string.score_board_empty))
-                } else {
-                    LazyColumn(
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(15.dp)
+                    .clip(shape)
+                    .background(colorScheme.tertiaryContainer, shape)
+            ) {
+                itemsIndexed(state.scores, key = { _, score -> score.id }) { _, score ->
+                    ListItem(
                         modifier = Modifier
-                            .fillMaxSize()
-                            .clip(RoundedCornerShape(5.dp))
-                    ) {
-                        itemsIndexed(state.scores, key = { _, score -> score.id }) { index, score ->
-                            ListItem(
-                                headlineContent = {
-                                    Row(
-                                        modifier = Modifier
-                                            .fillMaxWidth()
-                                            .padding(10.dp),
-                                        verticalAlignment = Alignment.CenterVertically,
-                                        horizontalArrangement = Arrangement.SpaceBetween
-                                    ) {
-                                        Text(text = score.name)
-                                        Text(text = score.winCount)
-                                    }
-                                },
-                                trailingContent = {
-                                    IconButton(
-                                        onClick = {
-                                            scoreBoardViewModel.delete(score.id)
-                                        }
-                                    ) {
-                                        Icon(imageVector = Icons.Default.Delete, contentDescription = null)
-                                    }
+                            .padding(5.dp)
+                            .clip(shape)
+                            .background(colorScheme.onTertiaryContainer, shape),
+                        headlineContent = {
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.SpaceBetween
+                            ) {
+                                Text(text = score.name)
+                                Text(text = score.winCount)
+                            }
+                        },
+                        trailingContent = {
+                            IconButton(
+                                onClick = {
+                                    scoreBoardViewModel.delete(score.id)
                                 }
-                            )
-                            if (index != state.scores.lastIndex) {
-                                Divider(
-                                    thickness = 2.dp,
-                                    color = colorScheme.secondaryContainer
-                                )
+                            ) {
+                                Icon(imageVector = Icons.Default.Delete, contentDescription = null)
                             }
                         }
-                    }
+                    )
                 }
             }
         }
--- LudoCompose/app/src/main/java/hu/bme/aut/android/ludocompose/features/local/scoreboard/ScoreBoardViewModel.kt
+++ LudoCompose/app/src/main/java/hu/bme/aut/android/ludocompose/features/local/scoreboard/ScoreBoardViewModel.kt
@@ -7,16 +7,11 @@ import hu.bme.aut.android.ludocompose.domain.usecases.DeleteScoreUseCase
 import hu.bme.aut.android.ludocompose.domain.usecases.LoadScoresUseCase
 import hu.bme.aut.android.ludocompose.ui.model.ScoreItemUi
 import hu.bme.aut.android.ludocompose.ui.model.toUiModel
-import hu.bme.aut.android.ludocompose.ui.model.toUiText
-import hu.bme.aut.android.ludocompose.ui.util.UiEvent
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.Channel
+import hu.bme.aut.android.ludocompose.ui.util.LoadingViewModel
+import hu.bme.aut.android.ludocompose.ui.util.UiEventViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -25,62 +20,43 @@ class ScoreBoardViewModel @Inject constructor(
     private val deleteScoreUseCase: DeleteScoreUseCase,
 ) : ViewModel() {
 
+    private val loadingViewModel = LoadingViewModel(
+        coroutineScope = viewModelScope,
+        loadData = ::loadData,
+    )
+
+    private val uiEventViewModel = UiEventViewModel(
+        coroutineScope = viewModelScope,
+        events = mapOf(
+            "delete" to ::deleteEvent,
+        ),
+    )
+
+    val loadingState get() = loadingViewModel.state
+
+    val uiEvent get() = uiEventViewModel.uiEvent
+
     private val _state = MutableStateFlow(ScoreBoardState())
     val state = _state.asStateFlow()
 
-    private val _uiEvent = Channel<UiEvent>()
-    val uiEvent = _uiEvent.receiveAsFlow()
-
-    init {
-        loadAll()
+    private suspend fun loadData() {
+        val games = loadScoresUseCase().getOrThrow().map { it.toUiModel() }
+        _state.update {
+            it.copy(scores = games)
+        }
     }
 
-    fun loadAll() {
-        viewModelScope.launch {
-            _state.update { it.copy(isLoading = true) }
-            try {
-                CoroutineScope(coroutineContext).launch(Dispatchers.IO) {
-                    val games = loadScoresUseCase().getOrThrow().map { it.toUiModel() }
-                    _state.update {
-                        it.copy(
-                            isLoading = false,
-                            scores = games,
-                        )
-                    }
-                }
-            } catch (e: Exception) {
-                _state.update {
-                    it.copy(
-                        isLoading = false,
-                        error = e,
-                    )
-                }
-            }
-        }
+    private suspend fun deleteEvent(data: Any?) {
+        val id = data as Long
+        deleteScoreUseCase(id)
+        loadingViewModel.load(true)
     }
 
     fun delete(id: Long) {
-        viewModelScope.launch {
-            try {
-                CoroutineScope(coroutineContext).launch(Dispatchers.IO) {
-                    deleteScoreUseCase(id)
-                    _state.update { state ->
-                        state.copy(
-                            scores = state.scores.filter { it.id != id },
-                        )
-                    }
-                }
-            } catch (e: Exception) {
-                _uiEvent.send(UiEvent.Failure(e.toUiText()))
-            }
-        }
+        uiEventViewModel.fire("delete", id)
     }
 }
 
 data class ScoreBoardState(
-    val isLoading: Boolean = false,
-    val error: Throwable? = null,
     val scores: List<ScoreItemUi> = emptyList(),
-) {
-    val isError: Boolean get() = error != null
-}
+)
