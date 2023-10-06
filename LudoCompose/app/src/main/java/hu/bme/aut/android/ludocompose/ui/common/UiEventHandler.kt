ec2daa8c91a4030c1e643120d9f07f9a5f47fd3d
Kis Benjamin <60401038+Kis-Benjamin@users.noreply.github.com>
2023-10-06 09:45:54 +0200

    Added UiEvent generalized handler

---
+++ LudoCompose/app/src/main/java/hu/bme/aut/android/ludocompose/ui/common/UiEventHandler.kt
@@ -0,0 +1,34 @@
+package hu.bme.aut.android.ludocompose.ui.common
+
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
+import hu.bme.aut.android.ludocompose.ui.util.UiEvent
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+
+@Composable
+fun UiEventHandler(
+    uiEvent: Flow<UiEvent>,
+    snackbarHostState: SnackbarHostState,
+) {
+    val scope = rememberCoroutineScope()
+
+    val context = LocalContext.current
+
+    LaunchedEffect(key1 = true) {
+        uiEvent.collect { uiEvent ->
+            when (uiEvent) {
+                is UiEvent.Success -> {}
+
+                is UiEvent.Failure -> {
+                    scope.launch {
+                        snackbarHostState.showSnackbar(uiEvent.message.asString(context))
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LudoCompose/app/src/main/java/hu/bme/aut/android/ludocompose/ui/util/UiEventViewModel.kt
@@ -0,0 +1,28 @@
+package hu.bme.aut.android.ludocompose.ui.util
+
+import hu.bme.aut.android.ludocompose.ui.model.toUiText
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+
+class UiEventViewModel(
+    private val coroutineScope: CoroutineScope,
+    private val events: Map<String, suspend (Any?) -> Unit>,
+) {
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun fire(event: String, data: Any? = null) {
+        coroutineScope.launch {
+            try {
+                launch(Dispatchers.IO) {
+                    events[event]!!.invoke(data)
+                }.join()
+            } catch (e: Exception) {
+                _uiEvent.send(UiEvent.Failure(e.toUiText()))
+            }
+        }
+    }
+}
