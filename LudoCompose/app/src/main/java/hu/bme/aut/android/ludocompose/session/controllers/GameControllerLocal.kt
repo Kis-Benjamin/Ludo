793a51af31bc72bd0b7c4566085c0991332d63dd
Kis Benjamin <60401038+Kis-Benjamin@users.noreply.github.com>
2023-10-12 11:19:45 +0200

    Added controller implementation

---
+++ LudoCompose/app/src/main/java/hu/bme/aut/android/ludocompose/session/controllers/GameControllerLocal.kt
@@ -0,0 +1,89 @@
+/*
+ * Copyright © 2023 Benjamin Kis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package hu.bme.aut.android.ludocompose.session.controllers
+
+import hu.bme.aut.android.ludocompose.domain.services.GameService
+import hu.bme.aut.android.ludocompose.session.converters.toSessionModel
+import hu.bme.aut.android.ludocompose.session.model.GameDto
+import hu.bme.aut.android.ludocompose.session.model.GameListItemDto
+import kotlinx.coroutines.delay
+import javax.inject.Inject
+
+class GameControllerLocal @Inject constructor(
+    private val gameService: GameService,
+) : GameController {
+
+    private var _id: Long? = null
+
+    private val id: Long
+        get() = checkNotNull(_id) { "No game loaded" }
+
+    override val hasActive: Boolean
+        get() = _id != null
+
+    override suspend fun getActive(): GameDto {
+        val game = gameService.get(id)
+        return game.toSessionModel()
+    }
+
+    override suspend fun start(playerNames: List<String>) {
+        require(playerNames.size in 2..4) {
+            "Number of players must be between 2 and 4"
+        }
+        require(playerNames.all { it.isNotBlank() }) {
+            "Player name must not be blank"
+        }
+        if (hasActive) {
+            delete(id)
+        }
+        val id = gameService.start(playerNames)
+        load(id)
+    }
+
+    override suspend fun getList(): List<GameListItemDto> {
+        val gameListItem = gameService.getAll()
+        return gameListItem.map { it.toSessionModel() }
+    }
+
+    override suspend fun load(id: Long) {
+        require(id > 0) { "Id must be positive" }
+        _id = id
+    }
+
+    override suspend fun unLoad() {
+        _id = null
+    }
+
+    override suspend fun save(name: String) {
+        require(name.isNotBlank()) { "Name must not be blank" }
+        require(!gameService.has(name)) { "Game save already exists:\n$name" }
+        gameService.save(id, name)
+    }
+
+    override suspend fun delete(id: Long) {
+        gameService.delete(id)
+        unLoad()
+    }
+
+    override suspend fun select() {
+        gameService.select(id)
+    }
+
+    override suspend fun step(): Boolean {
+        return gameService.step(id)
+    }
+}
---
+++ LudoCompose/app/src/main/java/hu/bme/aut/android/ludocompose/session/di/ControllerModule.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright © 2023 Benjamin Kis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package hu.bme.aut.android.ludocompose.session.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import hu.bme.aut.android.ludocompose.session.controllers.GameController
+import hu.bme.aut.android.ludocompose.session.controllers.GameControllerLocal
+import hu.bme.aut.android.ludocompose.session.controllers.ScoreController
+import hu.bme.aut.android.ludocompose.session.controllers.ScoreControllerLocal
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class ControllerModule {
+    @Binds
+    @Singleton
+    abstract fun bindGameController(
+        gameControllerLocal: GameControllerLocal
+    ): GameController
+
+    @Binds
+    @Singleton
+    abstract fun bindScoreController(
+        scoreControllerLocal: ScoreControllerLocal
+    ): ScoreController
+}
