587f7676d27db2822c2d75336d9cd5dc57c6cd2b
Kis Benjamin <60401038+Kis-Benjamin@users.noreply.github.com>
2023-10-23 20:52:13 +0200

    Removed old server files.

--- LudoSpringBoot/src/main/kotlin/hu/bme/aut/alkfejl/ludospringboot/data/datasources/TokenEntityRepository.kt
+++
@@ -1,6 +0,0 @@
-package hu.bme.aut.alkfejl.ludospringboot.data.datasources;
-
-import hu.bme.aut.alkfejl.ludospringboot.data.entities.TokenEntity
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface TokenEntityRepository : JpaRepository<TokenEntity, Int>
