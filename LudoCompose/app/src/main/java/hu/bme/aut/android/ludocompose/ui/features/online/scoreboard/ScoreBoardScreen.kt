1d5a089ea570b06232cdd047441385b617116bc8
Kis Benjamin <60401038+Kis-Benjamin@users.noreply.github.com>
2023-11-10 17:37:19 +0100

    Implemented online score board screen

--- LudoCompose/app/src/main/java/hu/bme/aut/android/ludocompose/ui/features/online/scoreboard/ScoreBoardScreen.kt
+++ LudoCompose/app/src/main/java/hu/bme/aut/android/ludocompose/ui/features/online/scoreboard/ScoreBoardScreen.kt
@@ -16,18 +16,15 @@
 
 package hu.bme.aut.android.ludocompose.ui.features.online.scoreboard
 
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
 import hu.bme.aut.android.ludocompose.ui.features.common.scoreboard.ScoreBoardScreen
 
 @Composable
 fun ScoreBoardScreen(
-    snackbarHostState: SnackbarHostState,
     scoreBoardViewModel: ScoreBoardViewModel = hiltViewModel()
 ) {
     ScoreBoardScreen(
-        snackbarHostState = snackbarHostState,
         scoreBoardViewModel = scoreBoardViewModel
     )
 }
