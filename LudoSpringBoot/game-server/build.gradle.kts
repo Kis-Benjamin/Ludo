2be743747638e93aa2d8c16ee24ef1420dd325b5
Kis Benjamin <60401038+Kis-Benjamin@users.noreply.github.com>
2023-11-10 15:30:25 +0100

    Added stomp messaging library

--- LudoSpringBoot/game-server/build.gradle.kts
+++ LudoSpringBoot/game-server/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-websocket")
+	implementation("org.springframework:spring-messaging")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.1")
