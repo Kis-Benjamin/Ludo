88dd94a9a5efabf7b4a3beb2a6106522189cc4c6
Kis Benjamin <60401038+Kis-Benjamin@users.noreply.github.com>
2023-11-17 22:10:25 +0100

    Added logging on the datasource level

--- LudoSpringBoot/game-server/src/main/kotlin/hu/bme/aut/alkfejl/ludospringboot/gameserver/data/datasource/UserRepositoryDatabase.kt
+++ LudoSpringBoot/game-server/src/main/kotlin/hu/bme/aut/alkfejl/ludospringboot/gameserver/data/datasource/UserRepositoryDatabase.kt
@@ -16,9 +16,12 @@
 
 package hu.bme.aut.alkfejl.ludospringboot.gameserver.data.datasource
 
+import hu.bme.aut.alkfejl.ludospringboot.gameserver.common.util.debug
+import hu.bme.aut.alkfejl.ludospringboot.gameserver.common.util.error
 import hu.bme.aut.alkfejl.ludospringboot.gameserver.data.dao.RoomEntityRepository
 import hu.bme.aut.alkfejl.ludospringboot.gameserver.data.dao.UserEntityRepository
 import hu.bme.aut.alkfejl.ludospringboot.gameserver.data.model.UserEntity
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 import kotlin.jvm.optionals.getOrNull
@@ -29,28 +32,48 @@ class UserRepositoryDatabase(
     private val userEntityRepository: UserEntityRepository,
 ) : UserRepository {
     override fun getAll(): List<UserEntity> {
-        return userEntityRepository.findAll()
+        val users = userEntityRepository.findAll()
+        logger debug "Users found: ${users.size}"
+        return users
     }
 
     override fun get(id: Long): UserEntity {
-        return userEntityRepository.findById(id).run {
-            requireNotNull(getOrNull()) { "User not found with id: $id" }
+        val user = userEntityRepository.findById(id).run {
+            requireNotNull(getOrNull()) { logger error "User not found with id: $id" }
         }
+        logger debug "User found with id: $id"
+        return user
     }
 
     override fun insert(user: UserEntity): UserEntity {
-        return userEntityRepository.save(user)
+        try {
+            return userEntityRepository.save(user)
+        } catch (e: IllegalArgumentException) {
+            throw IllegalArgumentException(logger error "User creation failed, id: ${user.id}", e)
+        }
     }
 
     override fun update(user: UserEntity) {
-        userEntityRepository.save(user)
+        try {
+            userEntityRepository.save(user)
+        } catch (e: IllegalArgumentException) {
+            throw IllegalArgumentException(logger error "User update failed, id: ${user.id}", e)
+        }
     }
 
     @Transactional
     override fun delete(user: UserEntity) {
-        val room = requireNotNull(user.room) { "User must be in a room" }
-        room.users.remove(user)
-        userEntityRepository.delete(user)
-        roomEntityRepository.save(room)
+        val room = requireNotNull(user.room) { logger error "User must be in a room" }
+        try {
+            room.users.remove(user)
+            userEntityRepository.delete(user)
+            roomEntityRepository.save(room)
+        } catch (e: IllegalArgumentException) {
+            throw IllegalArgumentException(logger error "User deletion failed, id: ${user.id}", e)
+        }
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(UserRepositoryDatabase::class.java)
     }
 }
